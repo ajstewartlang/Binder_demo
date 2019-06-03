847987f060a22d9dc190b149ec90f008aebe3ec4
Andrew Stewart <andrew.stewart@manchester.ac.uk>
2019-06-03 16:33:32 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,2 +1,4 @@
-install.packages("ggplot2")
-install.packages("dplyr")
+# We're not loading any additional packages but if we were to load gpglot2 (say) 
+# we would drop the following #
+
+#install.packages("ggplot2")
