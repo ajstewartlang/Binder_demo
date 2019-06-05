37f0c9ed1ef023adc0bd567fe644c94234d93a58
Andrew Stewart <andrew.stewart@manchester.ac.uk>
2019-06-05 15:39:45 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,6 +1,7 @@
 # We're not loading any additional packages but if we were to load gpglot2 (say) 
 # we would drop the following #
 
-#install.packages("ggplot2")
-
 install.packages("tidyverse")
+
+install.packages("ggplot2")
+
