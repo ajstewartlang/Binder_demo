dd80a60c16ca0c44a157e48ad9d483da596e7051
Andrew Stewart <andrew.stewart@manchester.ac.uk>
2019-06-07 13:38:20 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,6 +1,13 @@
 # Below add install.packages() for the installation of each package needed.
 # I'm installing ggplot2 here.
+
 # The runtime.txt file should ensure we pick up the version of ggplot2
-# associated with the date inthat file.
+# associated with the date in that file.
+
+# Note that if you are useing a Dockerfile with a pre-built version of R from
+# rocker/geospatial then the runtime.txt file is ignored.  If having a particular 
+# version of a package is more important than having a particular version
+# of R, then don't use Dockerfile as things currentlty stand but instead
+# build your Binder from scratch (even if it takes a while).
 
 install.packages("ggplot2")
