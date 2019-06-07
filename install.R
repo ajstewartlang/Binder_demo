116eb2c86a08187d6dad865b754c0ee6c2e6ac49
Andrew Stewart <andrew.stewart@manchester.ac.uk>
2019-06-07 10:20:14 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,6 +1,4 @@
 # Below add install.packages() for the installation of each package needed
 # I'm installing the entire tidyverse here
 
-install.packages("tidyverse")
-
 install.packages("ggplot2")
