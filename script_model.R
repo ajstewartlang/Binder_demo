b2c39fc6050f1879a6707e4a703da12f7c935b1f
Andrew Stewart <andrew.stewart@manchester.ac.uk>
2019-06-07 14:40:50 +0100

    Update script_model.R

--- script_model.R
+++ script_model.R
@@ -1,6 +1,6 @@
 # avoiding loading any additional packages - we're going base R all the way!
 
-# Let's examine where 
+# Let's examine the relationship between a car's fuel economy and its horsepower 
 plot(mtcars$hp, mtcars$mpg, main = "MPG vs. Horsepower for mtcars\ndataset", xlab = "Horsepower", ylab = "MPG")
 abline(lm(mtcars$mpg ~ mtcars$hp), col = "blue")
 
