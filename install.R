f772e704c9b298a5030c0dc3e212505bf0def3dc
Andrew Stewart <andrew.stewart@manchester.ac.uk>
2019-06-07 10:32:07 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,6 @@
-# Below add install.packages() for the installation of each package needed
-# I'm installing the entire tidyverse here
+# Below add install.packages() for the installation of each package needed.
+# I'm installing ggplot2 here.
+# The runtime.txt file should ensure we pick up the version of ggplot2
+# associated with the date inthat file.
 
 install.packages("ggplot2")
